c17c0ec1c1504278cfeeec63b0a698571a7328ff
Raffi Maulana Hadi <raffimaulana25@gmail.com>
2020-12-13 14:10:02 +0700

    Change encrypt to md5

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const app = express();
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -23,10 +24,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, {
-    secret: process.env.SECRET,
-    encryptedFields: ["password"]
-});
+
 
 
 const User = new mongoose.model("User", userSchema);
@@ -46,7 +44,7 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save((err) => {
@@ -60,7 +58,7 @@ app.post("/register", (req, res) => {
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({
         email: username
